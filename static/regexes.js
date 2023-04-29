b554ad802af60d798cd94b4aff3b980e16e7d5c8
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2023-04-30 09:32:04 +1000

    fixed link regexes so they escaped properly.

--- static/regexes.js
+++ static/regexes.js
@@ -63,9 +63,9 @@ updateRegexEmoji()
 function updateRegexLinks() {
     copyLinks.innerText = "Copy"
     if (inc_nonclick.checked) {
-        outputLinks.innerText = "(?:https?://)?[a-z0-9_\-\.]*[a-z0-9_\-]{2,}\.[a-z]{2,}";
+        outputLinks.innerText = "(?:https?://)?[a-z0-9_\\-\\.]*[a-z0-9_\\-]{2,}\\.[a-z]{2,}";
     } else {
-        outputLinks.innerText = "(?:https?://)[a-z0-9_\-\.]*[a-z0-9_\-]{2,}\.[a-z]{2,}";
+        outputLinks.innerText = "(?:https?://)[a-z0-9_\\-\\.]*[a-z0-9_\\-]{2,}\\.[a-z]{2,}";
     };
 };
 
@@ -82,7 +82,7 @@ updateRegexLinks();
 
 function updateRegexInlin() {
     copyInlin.innerText = "Copy"
-    outputInlin.innerText = "\[.*\]\((?:https?://)[a-z0-9_\-.]*[a-z0-9_-]{2,}\.[a-z]{2,}\)";
+    outputInlin.innerText = "\[.*\]\(<?(?:https?://)[a-z0-9_\\-.]*[a-z0-9_\\-]{2,}\\.[a-z]{2,}>?\)";
 };
 
 copyInlin.onclick = function() {
