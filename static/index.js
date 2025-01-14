f5a9048d9c0fab312f9d3db91cb9843be19463cb
treeben77 <77905642+treeben77@users.noreply.github.com>
2025-01-14 21:19:21 +1000

    update copyright year

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2023 - 2024 treeben77
+// Copyright (c) 2023 - 2025 treeben77
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
